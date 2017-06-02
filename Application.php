36380aa21b3f48a52c32207338450e3efefc943a
Emmanuel KWENE <njume48@gmail.com>
2017-06-02 03:06:20 +0100

    Starting the application development

---
+++ Application.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Emmanuel KWENE <njume48@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Plugino\Framework;
+
+
+class Application
+{
+
+
+	public function __construct()
+	{
+	}
+
+}
